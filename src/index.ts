155e1d396db3f7d74d3d659c3ac0348ff4569600
tyange <usun16@gmail.com>
2024-03-10 18:59:09 +0900

    add CsvFileReader

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import fs from "fs";
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("football.csv");
 reader.read();
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
 
 let manUnitedWins = 0;
 
