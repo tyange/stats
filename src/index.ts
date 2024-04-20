9fa9d3a58ca20e1bf36518d10e0d88263be9ad36
tyange <usun16@gmail.com>
2024-04-20 10:12:45 +0900

    add HtmlReport

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTarget/ConsoleReport";
+import { HtmlReport } from "./reportTarget/HtmlReport";
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader("football.csv");
@@ -12,9 +13,6 @@ const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport(),
-);
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
